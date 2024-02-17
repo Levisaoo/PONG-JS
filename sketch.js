6eac4eaa15ec269d303fdb346a2deddf42c31230
andrezin123123 <andrefarias123uva@gmail.com>
2024-02-17 20:52:02 -0300

    Revert "Facilitando  o Jogo"
    This reverts commit 13b235b3593295a772e32ed5965ab7b5cd272f0e.

--- sketch.js
+++ sketch.js
@@ -5,8 +5,8 @@ let diametro=18;
 let raio = diametro / 2 ;
 
   //velocidade da bolinha
-let = velocidadexBolinha = 9;
-let = velocidadeyBolinha = 9;
+let = velocidadexBolinha = 10;
+let = velocidadeyBolinha = 10;
 
 //variaveis da raquete;
 let xRaquete = 5;
