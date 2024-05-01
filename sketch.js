8fae4a1f0e1c3d23c5c3d2d5b5eff14e06ad23f5
Levisaoo <levi123uva@gmail.com>
2024-05-01 11:30:46 -0300

    Hard Game

--- sketch.js
+++ sketch.js
@@ -6,8 +6,8 @@ let raio = diametro / 2 ;
 
   //velocidade da bolinha
 
-let = velocidadexBolinha = 100;
-let = velocidadeyBolinha = 100;
+let = velocidadexBolinha = 2000;
+let = velocidadeyBolinha = 2000;
 
 
 //variaveis da raquete;
